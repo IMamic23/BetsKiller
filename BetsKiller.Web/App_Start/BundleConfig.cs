76cd82670182cba3fd1575969799a234babbb0ed
zw3ng <veran.pokornic@gmail.com>
2016-04-25 13:04:55 +0200

    Enabled JS and CSS minification; Fixed index_concrete.jpg path on landing page; Added google analytics script in layouts <head> element;

--- BetsKiller.Web/App_Start/BundleConfig.cs
+++ BetsKiller.Web/App_Start/BundleConfig.cs
@@ -102,8 +102,7 @@ namespace BetsKiller.Web
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            //BundleTable.EnableOptimizations = bool.Parse(ConfigurationManager.AppSettings["EnableOptimizations"].ToString());
-            BundleTable.EnableOptimizations = false;
+            BundleTable.EnableOptimizations = bool.Parse(ConfigurationManager.AppSettings["EnableOptimizations"].ToString());
         }
     }
 }
